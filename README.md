66ff5cbcb84aa7b88ec7b3f7f0da3fff97432338
Denis Piazentin <denis@piazentin.com>
2021-08-14 12:21:24 -0300

    Main entrypoint

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Backuper
 
 Very simple backup utility
+
+## Usage
+
+Create a new backup:
+
+```
+python3 -m backuper new ~/backup/source/dir ~/backup/destination/dir
+```
+
+Update existing backup:
+
+```
+python3 -m backuper update ~/backup/source/dir ~/backup/destination/dir
+```
+
+Check backup integrity:
+
+```
+python3 -m backuper check ~/backup/destination/dir
+```
--- backuper/__main__.py
+++ backuper/__main__.py
@@ -1,11 +1,21 @@
 import sys
 
+import backuper.argparser as parser
+from backuper.commands import NewCommand, UpdateCommand, CheckCommand
 import backuper.backup as bkp
 
 
 def run_with_args():
     print(sys.argv)
-    print('Gone')
+    command = parser.parse(sys.argv[1:])
+    if isinstance(command, NewCommand):
+        bkp.new(command)
+    elif isinstance(command, UpdateCommand):
+        bkp.update(command)
+    elif isinstance(command, CheckCommand):
+        bkp.check(command)
+    else:
+        raise ValueError(f'Unrecognized command {command}')
 
 
 if __name__ == "__main__":
