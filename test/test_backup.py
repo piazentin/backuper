8f6e7fbd3c8fddc9a437a13d227b78c9a9d967b7
Denis Piazentin <denis@piazentin.com>
2021-08-14 11:16:37 -0300

    Refactors test backup

--- test/test_backup.py
+++ test/test_backup.py
@@ -2,69 +2,78 @@ from backuper.commands import NewCommand, UpdateCommand
 import unittest
 from datetime import datetime
 import os
+import shutil
 from tempfile import gettempdir
 import csv
 import backuper.backup as bkp
 
 
 class BackupIntegrationTest(unittest.TestCase):
-    expected_hashs = ['fef9161f9f9a492dba2b1357298f17897849fefc',
-                      'cc2ff24e50730e1b7c238890fc877de269f9bd98']
-    expected_control_contents = [
-        ['f', 'text_file1.txt', 'fef9161f9f9a492dba2b1357298f17897849fefc'],
-        ['f', 'text_file1 copy.txt', 'fef9161f9f9a492dba2b1357298f17897849fefc'],
-        ['f', '/subdir/Original-Lena-image.png',
-            'cc2ff24e50730e1b7c238890fc877de269f9bd98'],
-        ['d', 'subdir', ''],
-        ['d', '/subdir/empty dir', ''],
-    ]
 
-    def _random_backup_dir(self):
-        return os.path.join(gettempdir(), 'backuper_integration_test', datetime.now().strftime("%Y-%m-%dT%H%M%S%f"))
+    new_backup = {
+        'source': './test/resources/bkp_test_sources_new',
+        'hashes': {'fef9161f9f9a492dba2b1357298f17897849fefc', 'cc2ff24e50730e1b7c238890fc877de269f9bd98'},
+        'meta': [
+            ['f', 'text_file1.txt', 'fef9161f9f9a492dba2b1357298f17897849fefc'],
+            ['f', 'text_file1 copy.txt', 'fef9161f9f9a492dba2b1357298f17897849fefc'],
+            ['f', '/subdir/Original-Lena-image.png',
+                'cc2ff24e50730e1b7c238890fc877de269f9bd98'],
+            ['d', 'subdir', ''],
+            ['d', '/subdir/empty dir', '']]
+    }
 
-    def test_new_backup(self):
-        source = './test/resources/bkp_test_sources_new'
-        bkp_destination = self._random_backup_dir()
-        bkp.new(NewCommand('testing', source, bkp_destination))
-
-        bkp_data_filenames = os.listdir(os.path.join(bkp_destination, 'data'))
-        self.assertEqual(len(bkp_data_filenames), len(self.expected_hashs))
-        for filename in bkp_data_filenames:
-            self.assertIn(filename, self.expected_hashs)
-
-        control_filename = os.path.join(bkp_destination, 'testing.csv')
-        with open(control_filename, 'r') as control_file:
-            for row in csv.reader(control_file):
-                self.assertIn(row, self.expected_control_contents)
-
-        self.assertTrue(True)
-
-    def test_update_backup(self):
-        new_source = './test/resources/bkp_test_sources_new'
-        update_source = './test/resources/bkp_test_sources_update'
-        expected_hashs = self.expected_hashs[:]
-        expected_hashs.append('7f2f5c0211b62cc0f2da98c3f253bba9dc535b17')
-        expected_control_contents = [
+    update_backup = {
+        'source': './test/resources/bkp_test_sources_update',
+        'hashes': new_backup['hashes'].union({'7f2f5c0211b62cc0f2da98c3f253bba9dc535b17'}),
+        'meta': [
             ['f', 'text_file1.txt', 'fef9161f9f9a492dba2b1357298f17897849fefc'],
             ['f', 'text_file1 copy.txt', '7f2f5c0211b62cc0f2da98c3f253bba9dc535b17'],
             ['d', 'subdir', ''],
-            ['d', '/subdir/empty dir', ''],
+            ['d', '/subdir/empty dir', '']
         ]
+    }
 
-        bkp_destination = self._random_backup_dir()
-        bkp.new(NewCommand('test_new', new_source, bkp_destination))
+    def setUp(self) -> None:
+        os.makedirs(os.path.join(gettempdir(), 'backuper_integration_test'), exist_ok=True)
+
+    def tearDown(self) -> None:
+        shutil.rmtree(os.path.join(gettempdir(), 'backuper_integration_test'))
+
+    def _random_dir(self):
+        return os.path.join(gettempdir(), 'backuper_integration_test', datetime.now().strftime("%Y-%m-%dT%H%M%S%f"))
+
+    def test_new_backup(self):
+        destination = self._random_dir()
+        bkp.new(NewCommand(
+            'testing', self.new_backup['source'], destination))
+
+        data_filenames = os.listdir(os.path.join(destination, 'data'))
+        self.assertEqual(len(data_filenames),
+                         len(self.new_backup['hashes']))
+        for filename in data_filenames:
+            self.assertIn(filename, self.new_backup['hashes'])
+
+        meta_filename = os.path.join(destination, 'testing.csv')
+        with open(meta_filename, 'r') as meta_file:
+            for row in csv.reader(meta_file):
+                self.assertIn(row, self.new_backup['meta'])
+
+    def test_update_backup(self):
+        destination = self._random_dir()
+        bkp.new(NewCommand('test_new', self.new_backup['source'], destination))
         bkp.update(UpdateCommand('test_update',
-                   update_source, bkp_destination))
+                   self.update_backup['source'], destination))
 
-        bkp_data_filenames = os.listdir(os.path.join(bkp_destination, 'data'))
-        self.assertEqual(len(bkp_data_filenames), len(expected_hashs))
-        for filename in bkp_data_filenames:
-            self.assertIn(filename, expected_hashs)
+        data_filenames = os.listdir(os.path.join(destination, 'data'))
+        self.assertEqual(len(data_filenames), len(
+            self.update_backup['hashes']))
+        for filename in data_filenames:
+            self.assertIn(filename, self.update_backup['hashes'])
 
-        control_filename = os.path.join(bkp_destination, 'test_update.csv')
-        with open(control_filename, 'r') as control_file:
-            for row in csv.reader(control_file):
-                self.assertIn(row, expected_control_contents)
+        meta_filename = os.path.join(destination, 'test_update.csv')
+        with open(meta_filename, 'r') as meta_file:
+            for row in csv.reader(meta_file):
+                self.assertIn(row, self.update_backup['meta'])
 
     def test_check_backup_name(self):
         # TODO
